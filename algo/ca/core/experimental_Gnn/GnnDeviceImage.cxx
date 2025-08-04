1906462a1f9f0eecb105f1491149495352497486
otyagi <oddharak.tyagi@gmail.com>
2025-08-04 17:50:39 +0200

    Added GnnTimeMonitor, removed xpu device image copy

--- algo/ca/core/experimental_Gnn/GnnDeviceImage.cxx
+++
@@ -1,7 +0,0 @@
-/* Copyright (C) 2025 FIAS Frankfurt Institute for Advanced Studies, Frankfurt / Main
-   SPDX-License-Identifier: GPL-3.0-only
-   Authors: Oddharak Tyagi [committer] */
-   #include "GnnDeviceImage.h"
-
-   XPU_IMAGE(cbm::algo::ca::GPUReco_Gnn);
-   
